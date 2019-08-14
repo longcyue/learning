60f81772402b2dcb0bb961f2731aec5ca5f3ebc9
mario <longcyue@github.com>
2019-08-14 23:56:03 +0800

    day5

---
+++ venv/day5.py
@@ -0,0 +1,11 @@
+# import collections
+# # collections 模块
+# import time
+# print(time.time())
+#
+# print(time.strftime("%Y-%m-%d %a %b %H:%M:%S"))
+# # "%Y-%m-%d %H:%M:%S" 表示：
+# print(time.localtime())
+#
+# import random
+print(random.randint(1,5))
