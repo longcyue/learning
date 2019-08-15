ec9d32d0e6e312d0e476109173c281d158896cdb
mario <longcyue@github.com>
2019-08-15 23:37:59 +0800

    day6

---
+++ venv/day6.py
@@ -0,0 +1,16 @@
+import json
+dic={1:'a',2:'b'}
+print(type(dic),dic)
+str_d = json.dumps(dic)
+print(type(str_d),str_d)
+
+
+
+f=open('ff','w',encoding='utf-8')
+json.dump(dic,f)
+f.close() #在本目录下新增了ff文件，dic内容被写入进去
+
+f=open('ff')
+res = json.load(f)
+f.close()
+print(type(res),res)
