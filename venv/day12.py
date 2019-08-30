23587f8e7ede274e276fc0a93be7dff18039063e
mario <longcyue@github.com>
2019-08-30 23:45:00 +0800

    day12

---
+++ venv/day12.py
@@ -0,0 +1,29 @@
+class Room:
+    def __init__(self, name, owner, width, length, high):
+        self.name = name
+        self.owner = owner
+        self.__width = width
+        self.__length = length
+        self.__high = high
+
+    def tell_area(self):  # 对外提供的接口，隐藏了内部的实现细节，此时我们想求的是面积
+        return self.__width * self.__length
+
+# 使用者
+r1 = Room('卧室', 'egon', 20, 20, 20)
+r1.tell_area()  # 使用者调用接口tell_area
+
+
+class Room:
+    def __init__(self, name, owner, width, length, high):
+        self.name = name
+        self.owner = owner
+        self.__width = width
+        self.__length = length
+        self.__high = high
+
+    def tell_area(self):  # 对外提供的接口，隐藏内部实现，此时我们想求的是体积,内部逻辑变了,只需求修该下列一行就可以很简答的实现,而且外部调用感知不到,仍然使用该方法，但是功能已经变了
+        return self.__width * self.__length * self.__high
+
+# 对于仍然在使用tell_area接口的人来说，根本无需改动自己的代码，就可以用上新功能
+r1.tell_area()
